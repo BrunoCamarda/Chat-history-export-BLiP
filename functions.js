8a96858841b44d857a09a7d41fedf06ae8f1dea9
brunocamarda <bruno_camarda@hotmail.com>
2019-09-12 15:10:11 -0300

    Add some features

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 var lastDate;
 var dates = [];
-var data; 
+var data;
+var token; 
 //Request 
 function getInfo() {
     document.getElementById("loader").style.display = "block";
@@ -11,7 +12,7 @@ function getInfo() {
     axios.post('https://msging.net/commands', {
         id: "4a3d0851-6a7f-4e69-8a43-f8598145866e",
         method: "get",
-        uri: "/threads"
+        uri: "/threads?$take=100"
     }, {
         headers: {
             'Authorization': token,
@@ -22,20 +23,21 @@ function getInfo() {
         data = data.sort(function (a, b) { //ordena por data
             return new Date(b.lastMessage.date) - new Date(a.lastMessage.date);
         });
-        populate(data, token); //Chama a funcao para criar a tabela
+        separateThreads(data, token); //Chama a funcao para criar a tabela
     });
 }
 
-function populate(data, token) {
+//Analisa cada thread separadamente e cruza as informacoes para receber os dados do contato
+function separateThreads(data, token) {
     for (var i = 0; i < data.length; i++) {
         lastDate = new Date(data[i].lastMessage.date);
-        console.log(lastDate);
         createDate(lastDate);
-        getUserInfo(data[i].identity, token, i);
+        getUserInfoToCreateTable(data[i].identity, token, i);
     }
 }
 
-function getUserInfo(user, token, i) {
+//chama a Extensao Contacts para pegar dados do Contato e criar uma tabela
+function getUserInfoToCreateTable(user, token, i) {
     axios.post('https://msging.net/commands', {
         id: (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase(),
         method: "get",
@@ -67,7 +69,7 @@ function createTable(user, i) {
         }
 
         td = tr.insertCell(tr.cells.length);
-        if (typeof(user.name) != "undefined") {
+        if (typeof (user.name) != "undefined") {
             td.innerHTML = "<strong>" + user.name + "</strong>";
         }
         if (user.source == "0mn.io") {
@@ -86,7 +88,7 @@ function createTable(user, i) {
 
         td = tr.insertCell(tr.cells.length);
         td.innerHTML = "<label class='bp-input--check--wrapper mb4'>" +
-            "<input class='bp-input' type='checkbox' onchange='enableExport()' name='checkbox-group' value=data.identity>" +
+            "<input class='bp-input' type='checkbox' onchange='enableExport()' name='checkbox-group' value=" + data[i].identity + ">" +
             "<div class='bp-input--checkbox'>&check;</div>" +
             "</label>";
     }
@@ -99,7 +101,7 @@ function createTable(user, i) {
 }
 
 //seleciona todos os checkboxes ao clicar no principal
-function toggle(source) { 
+function toggle(source) {
     checkboxes = document.getElementsByName('checkbox-group');
     for (var i = 0, n = checkboxes.length; i < n; i++) {
         checkboxes[i].checked = source.checked;
@@ -133,13 +135,73 @@ function stoppedTyping(input) {
     }
 }
 
-function enableExport() { //habilita e desabilita o botão de exportar caso haja pelo menos um checkbox selecionado
+//habilita e desabilita o botão de exportar caso haja pelo menos um checkbox selecionado
+function enableExport() {
     var checkboxes = document.getElementsByName('checkbox-group');
-    var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked); 
+    var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
     if (checkedOne) {
         document.getElementById('b-exportar').disabled = false;
-    }else{
+    } else {
         document.getElementById('b-exportar').disabled = true;
         document.getElementsByName('checkbox-all')[0].checked = false;
     }
-  }
+}
+
+function exportCsv(selectedRows) {
+    var checked = getElementsSelecteds();
+    console.log("passei");
+    const rows = [
+        ["ID do usuário", "Canal"]
+    ];
+
+    checked.forEach(function (item, index) {
+        rows[index + 1] = [];
+        rows[index + 1].push(item); //id do usuario
+        rows[index + 1].push("BLiP Chat");
+    });
+    console.log(rows);
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    rows.forEach(function (rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "threads-" + data[0].ownerIdentity + ".csv");
+    document.body.appendChild(link); // Required for FF
+
+    //link.click(); // Isso ira gerar o donwload do arquivo com o nome "{idBot}.csv".
+}
+
+//Pega sa linhas da tabela que forma selecionadas
+function getElementsSelecteds() {
+    var checkedValue = [];
+    var checkedElements = document.getElementsByName('checkbox-group');
+    for (var i = 0; checkedElements[i]; ++i) {
+        if (checkedElements[i].checked) {
+            checkedValue[i] = checkedElements[i].value;
+        }
+    }
+    return checkedValue;
+}
+
+function getUserInfo(user, token) {
+    axios.post('https://msging.net/commands', {
+        id: (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase(),
+        method: "get",
+        uri: "/contacts/" + user
+    }, {
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        if (response.data.status == "failure") {
+        } else {
+            let user = response.data.resource;
+            createTable(user, i);
+        }
+    });
+}
